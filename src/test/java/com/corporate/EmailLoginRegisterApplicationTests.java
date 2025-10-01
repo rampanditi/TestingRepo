2646403e9e73b9ed02b3df93e9e87c5aafdd8749
Ramakrishna <ramki4607@gmail.com>
2025-10-01 19:17:28 +0530

    Initial commit for email login register project

---
+++ src/test/java/com/corporate/EmailLoginRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailLoginRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
